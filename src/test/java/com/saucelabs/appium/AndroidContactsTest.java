2c3a52a07393dbc52ddfe8fca3cb5b8832a0d14c
yangfannjx <yangfannjx@gmail.com>
2014-04-30 23:22:53 +0800

    初始提交

---
+++ src/test/java/com/saucelabs/appium/AndroidContactsTest.java
@@ -0,0 +1,67 @@
+package com.saucelabs.appium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.HasTouchScreen;
+import org.openqa.selenium.interactions.TouchScreen;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteTouchScreen;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+
+public class AndroidContactsTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        // set up appium
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        //File appDir = new File(classpathRoot, "../../../apps/ContactManager");
+        //File app = new File(appDir, "ContactManager.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("device","Android");
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+        capabilities.setCapability(CapabilityType.VERSION, "4.2");
+        capabilities.setCapability(CapabilityType.PLATFORM, "MAC");
+        //capabilities.setCapability("app", app.getAbsolutePath());
+        capabilities.setCapability("app-package", "com.example.android.contactmanager");
+        capabilities.setCapability("app-activity", ".ContactManager");
+        driver = new SwipeableWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void addContact(){
+    }
+
+    @Test
+    public void addContact1(){
+    }
+
+
+    public class SwipeableWebDriver extends RemoteWebDriver implements HasTouchScreen {
+        private RemoteTouchScreen touch;
+
+        public SwipeableWebDriver(URL remoteAddress, Capabilities desiredCapabilities) {
+            super(remoteAddress, desiredCapabilities);
+            touch = new RemoteTouchScreen(getExecuteMethod());
+        }
+
+        public TouchScreen getTouch() {
+            return touch;
+        }
+    }
+
+
+}
+
